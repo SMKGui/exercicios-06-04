287e99c6aab466a5d80dbe9473d5dd4356d8e9f8
SMKGui <125401287+SMKGui@users.noreply.github.com>
2023-04-06 16:24:04 -0300

    Add files via upload

---
+++ Lista exercicios 06-04/Ex 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Faça um programa que receba um número inteiro e mostre a sua tabuada.
+
+Console.WriteLine($"Digite um numero para o calculo de sua tabuada de 1 a 10: ");
+int x = int.Parse(Console.ReadLine());
+int resultado = 0;
+
+for (int multiplicador = 0; multiplicador <= 10; multiplicador++)
+{
+    resultado = multiplicador * x;
+    Console.WriteLine($"{x} x {multiplicador} = {resultado}");
+}
---
+++ Lista exercicios 06-04/Ex 4/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Uma certa empresa fez uma pesquisa de mercado com 10 pessoas para saber se elas gostaram um determinado
+// produto lançado. Para isso forneceu o sexo do entrevistado e sua resposta (sim ou não). Faça um programa que calcule e imprima:
+// A.O número de pessoas que responderam SIM.
+// B.O número de pessoas que responderam NÃO;
+// C.O número de mulheres que responderam SIM;
+// D.A porcentagem de homens que responderam NÃO entre todos os homens analisados.
+
+int sim = 0;
+int nao = 0;
+
+//mulheres
+int msim = 0;
+int mnao = 0;
+
+Console.WriteLine($"Qual seu sexo? (f/m)");
+string sexo = Console.ReadLine();
+
+for ()
